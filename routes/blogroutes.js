275d68a9bd9b92dddb34abaf30a6e90fcf768034
manjeetverma <manjeetverma@gmail.com>
2023-03-28 19:18:40 +0530

    added 'isAuthor' middleware for dening unwanted user to edit or delete the blog server side and also hide the edit and delete button client side

--- routes/blogroutes.js
+++ routes/blogroutes.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const { blogschema } = require('../schemas.js');
 const Campground = require('../models/blog');
-const {isLoggedIn}=require('../middleware')
+const {isLoggedIn}=require('../middleware');
 
 
 const validateblog = (req, res, next) => {
@@ -15,6 +15,16 @@ const validateblog = (req, res, next) => {
     } else {
         next();
     }
+}
+
+const isAuthor=async(req,res,next)=>{
+    const {id}=req.params
+    const blog=await Campground.findById(id)
+    if(blog.author!==req.user._id){
+        req.flash('error','You do not have the permission')
+        return res.redirect(`/blogs/${blog._id}`)
+    }
+    next()
 }
 
 router.get('/', catchAsync(async (req, res) => {
@@ -29,7 +39,7 @@ router.get('/new',isLoggedIn, (req, res) => {
 })
 
 
-router.post('/', validateblog,isLoggedIn, catchAsync(async (req, res, next) => {
+router.post('/', validateblog,isLoggedIn,isAuthor, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const blog = new Campground(req.body.blog);
     blog.author=req.user._id
@@ -39,7 +49,7 @@ router.post('/', validateblog,isLoggedIn, catchAsync(async (req, res, next) => {
     res.redirect(`/blogs/${blog._id}`)
 }))
 
-router.get('/:id', catchAsync(async (req, res,) => {
+router.get('/:id',isLoggedIn, catchAsync(async (req, res,) => {
     const blog = await Campground.findById(req.params.id).populate('reviews').populate('author');//(people on discord)having trouble here
     if(!blog){
         req.flash('error','Blog not found')
@@ -58,7 +68,7 @@ router.get('/:id/edit',isLoggedIn, catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { blog });
 }))
 
-router.put('/:id',isLoggedIn, validateblog, catchAsync(async (req, res) => {
+router.put('/:id',isLoggedIn, validateblog,isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     const blog = await Campground.findByIdAndUpdate(id, { ...req.body.blog });
     req.flash('success','Successfully Updated the Blog')
@@ -67,7 +77,7 @@ router.put('/:id',isLoggedIn, validateblog, catchAsync(async (req, res) => {
 
 
 
-router.delete('/:id',isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id',isLoggedIn,isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);//this will trigger the findOneandDelete() middleware in the campground model.
     req.flash('success','Successfully deleted the blog')
